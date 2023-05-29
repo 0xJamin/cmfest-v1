e05168f09776c93d4550d4a931474a2f8144d8f9
Trust <okpusjamin@gmail.com>
2023-05-29 17:26:45 +0100

    feat: add responsiveness to herosection

--- components/sections/HeroSection/HeroSection.tsx
+++ components/sections/HeroSection/HeroSection.tsx
@@ -1,21 +1,22 @@
 import Button from "@/components/elements/Button";
 import photo from "../../../public/images/cm1.png";
 import photo2 from "../../../public/images/cm2.png";
-import photo3 from "../../../public/images/cm3.png";
+import photo3 from "../../../public/images/cm4.png";
+import photo4 from "../../../public/images/cm5.png";
 import Slider from "@/components/modules/Slider";
 import ImageSlide from "@/components/elements/ImageSlide";
 
 const HeroSection = ({ blok }: { blok: HeroType }) => {
   console.log(blok);
   const { label, heading, description, cta_buttons } = blok;
-  const sliderPhotos = [photo, photo2, photo3];
+  const sliderPhotos = [photo, photo2, photo3, photo4];
   return (
     <section>
       <div className="container-fluid max-w-screen-2xl mx-auto px-10">
-        <div className="bg-secondary rounded-4xl h-full py-8">
-          <div className="container max-w-screen-xl mx-auto p-8">
+        <div className="bg-secondary rounded-4xl h-full">
+          <div className="container max-w-screen-xl mx-auto">
             <div className="grid grid-cols-7 grid-flow-col gap-4">
-              <div className="col-span-4">
+              <div className="col-span-12 lg:col-span-7 p-14 text-center lg:text-left">
                 <span>{label}</span>
                 <h1 className="font-heading text-7xl font-bold leading-tight mb-5">
                   {heading}
@@ -27,7 +28,7 @@ const HeroSection = ({ blok }: { blok: HeroType }) => {
                   ))}
                 </div>
               </div>
-              <div className="clo-span-3">
+              <div className="hidden lg:block col-span-3">
                 <Slider>
                   {sliderPhotos.map((photo, id) => (
                     <ImageSlide image={photo} key={id} />
