871a49174157433f2f4948e987df62e389cde3e9
Trust <okpusjamin@gmail.com>
2023-05-29 17:12:48 +0100

    feat: add slider components

--- components/elements/ImageSlide/ImageSlide.tsx
+++ components/elements/ImageSlide/ImageSlide.tsx
@@ -1,26 +1,9 @@
 import Image, { StaticImageData } from "next/image";
-import { AnimatePresence, motion } from "framer-motion";
 
 export function ImageSlide({ image }: { image: string | StaticImageData }) {
   return (
-    <div>
-      <AnimatePresence>
-        <motion.div
-          animate={{ y: [0, -1000] }}
-          transition={{
-            delay: 2,
-            type: "damping",
-            duration: 10,
-            repeat: Infinity,
-            ease: "easeInOut",
-            repeatDelay: 1,
-          }}
-        >
-          <div className="image-slider relative p-4 m-7">
-            <Image src={image} alt="Slider photo" fill />
-          </div>
-        </motion.div>
-      </AnimatePresence>
+    <div className="image-slider relative p-4">
+      <Image src={image} alt="Slider photo" />
     </div>
   );
 }
--- components/modules/Slider/types.ts
+++ components/modules/Slider/types.ts
@@ -1,5 +1,3 @@
-import { StaticImageData } from "next/image";
-
 export type SliderType = {
-  photos: string[] | StaticImageData[];
+  children: JSX.Element[];
 };
