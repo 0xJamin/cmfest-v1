2f53f208b780905df4a72acfe911edc99ef258f2
Trust <okpusjamin@gmail.com>
2023-05-29 21:05:37 +0100

    fix: update styling to fix layout issues

--- components/elements/ImageSlide/ImageSlide.tsx
+++ components/elements/ImageSlide/ImageSlide.tsx
@@ -2,8 +2,8 @@ import Image, { StaticImageData } from "next/image";
 
 export function ImageSlide({ image }: { image: string | StaticImageData }) {
   return (
-    <div className="image-slider relative p-4">
-      <Image src={image} alt="Slider photo" />
+    <div className="image-slider relative p-4 ">
+      <Image src={image} alt="Slider photo" className="shadow-xl" />
     </div>
   );
 }
