a9f4f2a133ae9279b7e910c9168e394ee9cb8362
Trust <okpusjamin@gmail.com>
2023-05-30 11:51:27 +0100

    refactor: change png icon imports to SVGs

--- components/elements/Icons/Icon.tsx
+++
@@ -1,8 +0,0 @@
-// import clsx from "clsx"
-// import FaMenu from "react-icons/fa";
-
-// export const Icon = ({ className }:  Element) => {
-//   return (
-//     <FaMenu className={clsx(className)} />
-//   )
-// }
--- components/layout/Footer/Footer.tsx
+++ components/layout/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
-import TW from "../../../public/twitter.png";
-import IG from "../../../public/instagram.png";
-import IN from "../../../public/linkedin.png";
+import TW from "../../../public/twitter.svg";
+import IG from "@/public/instagram.svg";
+import IN from "../../../public/linkedin.svg";
 import Image from "next/image";
 
 export function Footer() {
@@ -13,55 +13,39 @@ export function Footer() {
   ];
 
   return (
-    <footer className="container-fluid w-full mx-auto fixed bottom-0 text-white left-0 bg-primary px-10 shadow-md">
-      <div className="flex flex-col md:flex-row items-center gap-4 md:items-center md:justify-between max-w-screen-2xl py-5 pt-8 md:pt-0">
-        <div className="md:flex grid grid-cols-1 text-center gap-4 md:items-center md:justify-center text-[17px]">
-          <div className="hidden md:inline-block mr-0 md:mr-32">
-            <p>&copy; 2023 CMFEST 2023, Inc. All rights reserved</p>
+    <footer className=" w-full mx-auto bottom-0 text-white bg-primary p-8 mt-5 ">
+      <div className="container  max-w-screen-xl mx-auto">
+        <div className="flex flex-col md:flex-row items-center gap-4 md:items-center md:justify-between max-w-screen-2xl py-5 pt-8 md:pt-0">
+          <div className="md:flex grid grid-cols-1 text-center gap-4 md:items-center md:justify-center text-sm font-light">
+            <div className=" mr-0 md:mr-32">
+              <p className="">
+                &copy; {new Date().getFullYear()} CMFEST Africa. All rights
+                reserved
+              </p>
+            </div>
+            <div className="flex flex-col md:flex-row text-center md:items-center md:justify-between">
+              {menu.map((name) => (
+                <Link
+                  className="mr-0 md:mr-4 text-[#7D7E8C] "
+                  key={name.name}
+                  href={name.link}
+                >
+                  <span className="">{name.name}</span>
+                </Link>
+              ))}
+            </div>
           </div>
-          <div className="flex flex-col md:flex-row text-center md:items-center md:justify-between">
-            {menu.map((name) => (
-              <Link
-                className="mr-0 md:mr-4 text-[#7D7E8C]"
-                key={name.name}
-                href={name.link}
-              >
-                <span className="">{name.name}</span>
-              </Link>
-            ))}
+          <div className="flex gap-4">
+            <Link href="https://twitter.com/cmfestafrica">
+              <Image src={TW} alt="Twitter Icon" className="w-6 h-6" />
+            </Link>
+            <Link href="https://www.instagram.com/cmfestafrica/">
+              <Image src={IG} alt="Instagram Icon" className="w-6 h-6" />
+            </Link>
+            <Link href="https://www.linkedin.com/company/cmfestafrica">
+              <Image src={IN} alt="LinkedIN Icon" className="w-6 h-6" />
+            </Link>
           </div>
-        </div>
-        <div className="flex gap-4">
-          <Link href="https://twitter.com/cmfestafrica">
-            <Image
-              src={TW}
-              alt="Twitter Icon"
-              width={undefined}
-              height={undefined}
-              className="w-6 h-6"
-            />
-          </Link>
-          <Link href="https://www.instagram.com/cmfestafrica/">
-            <Image
-              src={IG}
-              alt="Twitter Icon"
-              width={undefined}
-              height={undefined}
-              className="w-6 h-6"
-            />
-          </Link>
-          <Link href="https://www.linkedin.com/company/cmfestafrica">
-            <Image
-              src={IN}
-              alt="Twitter Icon"
-              width={undefined}
-              height={undefined}
-              className="w-6 h-6"
-            />
-          </Link>
-        </div>
-        <div className="md:hidden text-sm">
-          <p>&copy; 2023 CMFEST 2023, Inc. All rights reserved</p>
         </div>
       </div>
     </footer>
